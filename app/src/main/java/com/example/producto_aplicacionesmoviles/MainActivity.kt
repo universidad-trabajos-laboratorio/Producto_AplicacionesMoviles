5293f4276084cdda9ef1b0a44f892e895b213e4d
AbelLlontopMeza <a.llontopmeza@gmail.com>
2022-08-20 16:27:47 -0500

    creacion del proyecto

---
+++ app/src/main/java/com/example/producto_aplicacionesmoviles/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.producto_aplicacionesmoviles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
