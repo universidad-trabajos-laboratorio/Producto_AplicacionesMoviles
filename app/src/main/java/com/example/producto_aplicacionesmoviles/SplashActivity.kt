868a2a670a7cada20b2523e0359609a34ffbb4b2
AbelLlontopMeza <a.llontopmeza@gmail.com>
2022-08-21 22:20:22 -0500

    Reconfiguracion Inicial

--- app/src/main/java/com/example/producto_aplicacionesmoviles/SplashActivity.kt
+++ app/src/main/java/com/example/producto_aplicacionesmoviles/SplashActivity.kt
@@ -1,12 +1,12 @@
 package com.example.producto_aplicacionesmoviles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        Thread.sleep(5000)
-        setContentView(R.layout.activity_main)
+        startActivity(Intent(this,MainActivity::class.java))
     }
 }
